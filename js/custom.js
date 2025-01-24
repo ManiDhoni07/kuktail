4afe1cac78db4a10754b80998f82483a161260f3
mani <mani99ece@gmail.com>
2025-01-24 10:50:46 +0530

    updated changes

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,9 @@ $('#bannerslide').owlCarousel({
     margin:10,
     nav:false,
     autoplay:true,
-    autoplayTimeout:1000,
+    autoplaySpeed:2000,
+    autoplayTimeout:2000,
+    slideTransition: 'linear',
     autoplayHoverPause:true,
     responsive:{
         0:{
@@ -19,13 +21,14 @@ $('#bannerslide').owlCarousel({
 })
 
 $('#testimonialslide, .category-slide, #styleslide').owlCarousel({
-    loop:true,
+    loop:false,
     margin:10,
-    nav:false,
+    nav:true,
     autoplay:false,
     responsive:{
         0:{
-            items:1.2
+            items:1.2,
+            nav:false
         },
         600:{
             items:2.2
@@ -35,6 +38,38 @@ $('#testimonialslide, .category-slide, #styleslide').owlCarousel({
         }
     }
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let toggleButton = document.querySelector("[data-bs-toggle='offcanvas']");
+    let offcanvasElement = document.getElementById("navbarNav");
+
+    offcanvasElement.addEventListener("hide.bs.offcanvas", function () {
+        toggleButton.classList.remove("active"); 
+    });
+
+    offcanvasElement.addEventListener("show.bs.offcanvas", function () {
+        toggleButton.classList.add("active"); 
+    });
+
+
+    let lastScrollTop = 0;
+    const header = document.getElementById("header");
+
+    window.addEventListener("scroll", function () {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop && scrollTop > 150) {
+            header.classList.remove("sticky");
+        } else if (scrollTop < lastScrollTop && scrollTop > 150) {
+            header.classList.add("sticky");
+        }else{
+            header.classList.remove("sticky");
+        }
+
+        lastScrollTop = scrollTop;
+    });
+});
 
 
 
